6e64abeef62204572109a094e4409a64b2562e24
AlexKreez <37457955+AlexKreez@users.noreply.github.com>
2025-02-25 11:36:34 +0300

    Update TemplateRepository.java

--- src/main/java/com/example/demo/repository/TemplateRepository.java
+++ src/main/java/com/example/demo/repository/TemplateRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Long> {
-    // Дополнительные методы поиска по имени и т.п. можно добавить здесь.
 }
